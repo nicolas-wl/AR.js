96092433795f9d663fdfee6aa92c3c451f95bd47
Nicolas <nicolas@weblion.be>
2021-12-10 14:29:45 +0100

    aspectRatio fix

--- aframe/build/aframe-ar.js
+++ aframe/build/aframe-ar.js
@@ -5981,58 +5981,66 @@ AFRAME.registerComponent('arjs-look-controls', {
     this.hasSavedPose = false;
   }
 });
-AFRAME.registerComponent('arjs-webcam-texture', {
+AFRAME.registerComponent("arjs-webcam-texture", {
+  init: function () {
+    this.scene = this.el.sceneEl;
+    this.texCamera = new THREE.OrthographicCamera(-0.5, 0.5, 0.5, -0.5, 0, 10);
+    this.texScene = new THREE.Scene();
 
-    init: function() {
-        this.scene = this.el.sceneEl;
-        this.texCamera = new THREE.OrthographicCamera(-0.5, 0.5, 0.5, -0.5, 0, 10);
-        this.texScene = new THREE.Scene();
+    this.scene.renderer.autoClear = false;
+    this.video = document.createElement("video");
+    this.video.setAttribute("autoplay", true);
+    this.video.setAttribute("playsinline", true);
+    this.video.setAttribute("display", "none");
+    document.body.appendChild(this.video);
+    this.geom = new THREE.PlaneBufferGeometry(); //0.5, 0.5);
+    this.texture = new THREE.VideoTexture(this.video);
+    this.material = new THREE.MeshBasicMaterial({ map: this.texture });
+    const mesh = new THREE.Mesh(this.geom, this.material);
+    this.texScene.add(mesh);
+  },
 
-        this.scene.renderer.autoClear = false;
-        this.video = document.createElement("video");
-        this.video.setAttribute("autoplay", true);
-        this.video.setAttribute("playsinline", true);
-        this.video.setAttribute("display", "none");
-        document.body.appendChild(this.video);
-        this.geom = new THREE.PlaneBufferGeometry(); //0.5, 0.5);
-        this.texture = new THREE.VideoTexture(this.video);
-        this.material = new THREE.MeshBasicMaterial( { map: this.texture } );
-        const mesh = new THREE.Mesh(this.geom, this.material);
-        this.texScene.add(mesh);
-    },
-
-    play: function() {
-        if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-            const constraints = { video: {
-                facingMode: 'environment' }
-            };
-            navigator.mediaDevices.getUserMedia(constraints).then( stream=> {
-                this.video.srcObject = stream;    
-                this.video.play();
-            })
-            .catch(e => { alert(`Webcam error: ${e}`); });
-        } else {
-            alert('sorry - media devices API not supported');
-        }
-    },
-
-    tick: function() {
-        this.scene.renderer.clear();
-        this.scene.renderer.render(this.texScene, this.texCamera);
-        this.scene.renderer.clearDepth();
-    },
-
-    pause: function() {
-        this.video.srcObject.getTracks().forEach ( track => {
-            track.stop();
+  play: function () {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      const constraints = {
+        video: {
+          facingMode: "environment",
+          aspectRatio: 16 / 9,
+          width: { min: 1024, ideal: 1280, max: 1920 },
+          height: { min: 576, ideal: 720, max: 1080 },
+        },
+      };
+      navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then((stream) => {
+          this.video.srcObject = stream;
+          this.video.play();
+        })
+        .catch((e) => {
+          alert(`Webcam error: ${e}`);
         });
-    },
-
-    remove: function() {
-        this.material.dispose();
-        this.texture.dispose();
-        this.geom.dispose();
+    } else {
+      alert("sorry - media devices API not supported");
     }
+  },
+
+  tick: function () {
+    this.scene.renderer.clear();
+    this.scene.renderer.render(this.texScene, this.texCamera);
+    this.scene.renderer.clearDepth();
+  },
+
+  pause: function () {
+    this.video.srcObject.getTracks().forEach((track) => {
+      track.stop();
+    });
+  },
+
+  remove: function () {
+    this.material.dispose();
+    this.texture.dispose();
+    this.geom.dispose();
+  },
 });
 /*
  * UPDATES 28/08/20:
